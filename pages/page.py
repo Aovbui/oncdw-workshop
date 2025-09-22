b6e43e19241522739f4a190afdb58d7e3a20ea98
Alice Bui <aovbui@uvic.ca>
2025-09-22 15:34:23 -0700

    first commit

---
+++ pages/page.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 14:32:51 2025
+
+@author: aovbui
+"""
+
+import streamlit as st
+
+st.title('Hello, Streamlit! 👋')
+st.write('Welcome to my :red[first] web app!')
+
+
+
